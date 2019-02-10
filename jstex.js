de1ea976280117f84c8a00cc6ed6c5d666c85f43
Vegard Ekblad Itland <Vegard.Itland@student.uib.no>
2019-02-10 17:09:17 +0100

    Update JS code to work with new example

--- jstex.js
+++ jstex.js
@@ -1,3 +1,7 @@
+// global regex
+let regtexnc = new RegExp(Object.keys(mapsym).sort((a,b) => a.length<b.length).reduce((acc, x) => acc + x + "|", "\\\\(?:").slice(0, -1) + ")", 'g');
+let regtexc = new RegExp(Object.keys(mapsym).sort((a,b) => a.length<b.length).reduce((acc, x) => acc + x + "|", "\\\\(").slice(0, -1) + ")", 'g');
+
 let jstex = (function(){
     // Source: https://stackoverflow.com/a/4835406/4498826
     let escapeHtml = text => {
@@ -12,12 +16,10 @@ let jstex = (function(){
         return text.replace(/[&<>"']/g, function(m) { return map[m]; });
     };
     
-    let rstr = Object.keys(mapsym).reduce((acc, x) => acc + x + "|", "\\\\(").slice(0, -1) + ")";
-    let regex = new RegExp(rstr, 'g');
     let map = m => { return mapsym[m.slice(1)]; };
     return (text, htmlescape) => {
         if (htmlescape)
             text = escapeHtml(text);
-        return text.replace(regex, map);
+        return text.replace(regtexnc, map);
     };
 })();
